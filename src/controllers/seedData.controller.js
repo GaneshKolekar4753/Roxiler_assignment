3e23025cb2ddb7ccd9ed9fa35bb72969c93a723b
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-07-10 13:05:15 +0530

    seed data inti db

---
+++ src/controllers/seedData.controller.js
@@ -0,0 +1,16 @@
+import Product from "../models/productModel.js";
+
+//fetch the JSON from the third party API and initialize the database with seed data.
+export const getAllData = async (req, res) => {
+  try {
+    const response = await fetch("https://s3.amazonaws.com/roxiler.com/product_transaction.json", {
+      method: "GET",
+    });
+    const data = await response.json();
+    await Product.deleteMany();
+    await Product.insertMany(data);
+    res.status(200).json({ msg: "DB seeded successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "internal server error", err: error });
+  }
+};
